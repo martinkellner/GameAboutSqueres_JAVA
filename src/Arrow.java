d0bb14ef47eb2131d3190e9c0ed5006d962f1bd6
martinkellner <martin.kellner.1@gmail.com>
2016-05-26 16:45:49 +0200

    Beginning

---
+++ src/Arrow.java
@@ -0,0 +1,17 @@
+import javafx.scene.shape.Circle;
+import javafx.scene.image.Image;
+/**
+ * Created by martin on 26.5.2016.
+ */
+public class Arrow extends Circle{
+
+    private char direction;
+    private Image image;
+    private double size;
+
+    public Arrow(double i, double j, char direction){}
+
+    public char getDirection(){return direction;};
+    private void setRotation(){};
+
+}
