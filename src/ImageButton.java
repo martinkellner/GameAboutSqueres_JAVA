ea84ac254e2a0e9c026482be1e95b86f1906f0ca
martinkellner <martin.kellner.1@gmail.com>
2016-05-27 21:48:21 +0200

    add ImageButton

--- src/ImageButton.java
+++ src/ImageButton.java
@@ -1,16 +1,31 @@
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * Created by martin on 26.5.2016.
  */
 public class ImageButton extends Button {
 
-    private String STYLE_NORMAL0;
-    private String STYLE_NORMAL1;
-    private String STYLE_PRESSED;
-    private String text;
+    private String STYLE_NORMAL0 = "-fx-background-color: transparent;";
+    private String STYLE_NORMAL1 = "-fx-background-color: green;";
+    private String STYLE_PRESSED = "-fx-background-color:transparent;";
 
-    public ImageButton(java.lang.String imageurl, double size, java.lang.String text){
+
+    public ImageButton(String imageurl, double size, String text){
+
+        setHeight(size);
+        setWidth(size);
+
+        if (imageurl != "") {
+            Image image = new Image(getClass().getResourceAsStream(imageurl));
+            setStyle(STYLE_NORMAL0);
+            setGraphic(new ImageView(image));
+        }
+        else{
+            setStyle(STYLE_NORMAL1);
+            setText(text);
+        }
     }
 
     public String getSt(){
